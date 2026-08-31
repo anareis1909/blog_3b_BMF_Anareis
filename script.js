9ff60f06f078a70c9517a27f593ee4bf12165451
anareis1909 <ana.reis.azevedo@escola.pr.gov.br>
2026-08-31 11:58:30 -0300

    salvando

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () =>{
+prepararReacoes();
+prepararAnimacaoCard();
+criarBotaoTopo();
+})
+
+function prepararReacoes() {
+const artigos = document.querySelectorAll("article");
+
+artigos.forEach((artigo, indice) =>{
+
+ })
+}
